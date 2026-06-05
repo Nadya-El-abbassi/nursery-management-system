8dda50843dfce2a9f5ff2e1a1337840139125cad
Douaa Mouyassir <d.mouyassir@mundiapolis.ma>
2026-06-05 21:07:31 +0100

    ajout de service category CRUD

--- app/repositories/category_repository.py
+++ app/repositories/category_repository.py
@@ -16,3 +16,32 @@ class CategoryRepository:
             "SELECT * FROM categories WHERE id = ?",
             (category_id,),
         ).fetchone()
+
+    def create(self, name, description=""):
+        db = get_db()
+        cursor = db.execute(
+            """
+            INSERT INTO categories (name, description)
+            VALUES (?, ?)
+            """,
+            (name, description),
+        )
+        db.commit()
+        return cursor.lastrowid
+
+    def update(self, category_id, name, description=""):
+        db = get_db()
+        db.execute(
+            """
+            UPDATE categories
+            SET name = ?, description = ?
+            WHERE id = ?
+            """,
+            (name, description, category_id),
+        )
+        db.commit()
+
+    def delete(self, category_id):
+        db = get_db()
+        db.execute("DELETE FROM categories WHERE id = ?", (category_id,))
+        db.commit()
